b25cbdf2a45cca77227e8a1885cb1a9f07f8b78b
Adam Kahl <48374384+Icyadam14@users.noreply.github.com>
2022-03-28 10:02:07 -0400

    Initial commit

---
+++ project2.m
@@ -0,0 +1,22 @@
+
+% VARIABLES
+g = 9.8;
+p_air = 0;
+p_particle = 0;
+
+% Distance from the place
+diameter = 0;
+% Stokes constant
+c_d = 0;
+% 
+v_vert = 0;
+
+
+charge = 0;
+charge_density = 0;
+
+k = 0;
+k_0 = 8.85e-12;
+
+% Vertical distance from the plate
+vert_distance = 0;
